b390a90d40e7d45271c07b506a8ccc7800d04e57
Leo Rapini <leorapini@gmail.com>
2022-09-29 17:25:09 -0300

    add http client and adapter

---
+++ lib/infra/http_adapter.dart
@@ -0,0 +1,35 @@
+import 'package:http/http.dart';
+
+import '../data/http/http_client.dart';
+import '../data/http/http_error.dart';
+
+class HttpAdapter implements HttpClient {
+  final Client client;
+
+  HttpAdapter(this.client);
+
+  @override
+  Future<Response> request({
+    required String url,
+  }) async {
+    late Response res;
+    try {
+      res = await client.get(Uri.parse(url));
+    } catch (e) {
+      throw HttpError.serverError;
+    }
+    if (res.statusCode == 200) {
+      return res;
+    } else if (res.statusCode == 400) {
+      throw HttpError.badRequest;
+    } else if (res.statusCode == 401) {
+      throw HttpError.unauthorized;
+    } else if (res.statusCode == 403) {
+      throw HttpError.forbidden;
+    } else if (res.statusCode == 404) {
+      throw HttpError.notFound;
+    } else {
+      throw HttpError.serverError;
+    }
+  }
+}
